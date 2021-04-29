1c6ae32511fd4bf0903064e8e86ad1a96104c8e4
Steven Wu <steven.wu@kiwiplates.nz>
2021-04-29 23:40:58 +1200

    update example

--- src/plugin.js
+++ src/plugin.js
@@ -16,18 +16,18 @@ const plugin = {
     const ctx = chart.ctx;
 
     const fontSize = (height / 114).toFixed(2);
-    if(centerText.fontSizeAdjust){
-      fontSize = fontSize * ( 1 + centerText.fontSizeAdjust) ;
+    if (centerText.fontSizeAdjust) {
+      fontSize = fontSize * (1 + centerText.fontSizeAdjust);
     }
     ctx.font = fontSize + "em sans-serif";
     ctx.textBaseline = "middle";
     const textX = Math.round((width - ctx.measureText(centerText.value).width) / 2 + left);
 
     const textY = Math.round(height / 2 + top);
-    if(centerText.color){
-      ctx.fillStyle = centerText.color
+    if (centerText.color) {
+      ctx.fillStyle = centerText.color;
     }
-    ctx.fillText(innerText, textX, textY);
+    ctx.fillText(centerText.value, textX, textY);
   }
 };
 
